307f41f2aa7345b9245b6aa1e16a6fac32998999
ASDAFF <asdaff.asad@yandex.ru>
2019-07-22 04:23:22 +0500

    up

--- install/version.php
+++ install/version.php
@@ -1,6 +1,10 @@
 <?
+/**
+ * Copyright (c) 22/7/2019 Created By/Edited By ASDAFF asdaff.asad@yandex.ru
+ */
+
 $arModuleVersion = array(
-	"VERSION" => "1.1.7",
-	"VERSION_DATE" => "2018-11-13 00:00:00"
+	"VERSION" => "1.0",
+	"VERSION_DATE" => "2019-07-22 00:00:00"
 );
 ?>
